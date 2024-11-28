129b97f8f4765b3ccbbb49e5552169486ad9f0c0
Ajay Kumar Jagu <90963171+ajay6034@users.noreply.github.com>
2024-11-28 18:48:06 -0500

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv() ## used to load the environment variables
 import os
 
-GOOGLE_API_KEY= "AIzaSyAy-Gd3MMXEEA6_F3pw_BR3uQO7T4sZRHQ"
+GOOGLE_API_KEY= "put_api_key"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 ## function to load the model and get response
